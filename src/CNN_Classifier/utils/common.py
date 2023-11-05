90178ef98a6be201a99b3c4dd8fbfb03e0d7b678
vivek-yogi <101085320+vivek-yogi@users.noreply.github.com>
2023-11-05 18:07:06 +0530

    Utils functions are added

---
+++ src/CNN_Classifier/utils/common.py
@@ -0,0 +1,85 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from CNN_Classifier import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml) -> ConfigBox:
+    """ reads yaml file and returns 
+
+    Args: path_to_yaml(str): path like input
+
+    Raises:
+        value error : if yaml file is empty
+        e           : empty file
+    
+    Returns : 
+        configBox : ConfigBox
+    """ 
+
+    try: 
+        with open (path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+
+            return ConfigBox(content)
+        
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+    
+
+    @ ensure_annotations
+    def create_directories(path_to_directories:list,verbose=True):
+        """
+        create list of directories
+        Args: path_to_directories(list): list of path of directories
+        ignore_log(bool,optional): ignore if multiple dirs is to be created
+
+        """
+        for path in path_to_directories:
+            os.makedirs(path,exist_ok=True)
+            if verbose:
+                logger.info(f"created directory at :{path}")
+
+
+    @ ensure_annotations
+    def load_json(path = Path) ->ConfigBox:
+        """
+        load json files data
+        Args: path(Path): path to json files
+        Returns: ConfigBox: data as class attributes instead of dict
+        """
+
+        with open(path) as f:
+            content = json.load(f)
+            logger.info(f"json file loaded successfully from : {path}")
+            return ConfigBox(content)
+        
+    
+    @ ensure_annotations
+    def save_json(path:Path,data:dict):
+        """ save json data
+        Args: path(Path): path to json file
+              data(dict): data to saved in json file 
+        """
+        with open (path,'w') as f:
+            json.dump(data,f,indent=4)
+        logger.info(f"json file saved at : {path}")
+
+
+    @ ensure_annotations
+    def get_size(path:Path) -> str:
+        """ get size in KB
+        Args: path(Path): path of the file
+        Returns: str : Size in KB
+        """
+        size_in_KB = round(os.path.getsize(Path)/1024)
+        return f"~ {size_in_KB} KB"
---
+++ test.py
@@ -0,0 +1,2 @@
+from CNN_Classifier import logging
+logging.info("Welome to our custom log")
